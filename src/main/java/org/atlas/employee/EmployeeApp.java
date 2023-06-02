dab686df377115043a9c864f1424d5642c2c3a44
HabtamuTesfie <95411136+HabtamuTesfie@users.noreply.github.com>
2023-06-02 04:35:15 -0400

    updating readme file

--- src/main/java/org/atlas/employee/EmployeeApp.java
+++ src/main/java/org/atlas/employee/EmployeeApp.java
@@ -24,10 +24,9 @@ import java.util.logging.Logger;
 //------------------------------------------------------------------------------
 public class EmployeeApp
 {
-    static Logger logger = Logger.getLogger(String.valueOf(EmployeeApp.class));
-
-    public static void main(String[] args)
-    {
-      logger.info("Application is running");
-    }
+  static Logger logger = Logger.getLogger(String.valueOf(EmployeeApp.class));
+  public static void main(String[] args)
+  {
+    logger.info("Application is running");
+  }
 } // EmployeeApp
